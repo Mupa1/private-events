2796cde31315f775f1d57f2bb8686af6f852b119
Mupa1 <mupasmail@gmail.com>
2020-07-08 15:47:54 +0200

    refactor event to upcoming and previous and add navigation view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @events = @user.events
+    @previous_events = current_user.previous_events
+    @upcoming_events = current_user.upcoming_events
   end
 
   private
