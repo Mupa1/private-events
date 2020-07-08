29255bf34a5775f2345153ee3d26c8369974cd30
taiwocoker <taiwofcoker@gmail.com>
2020-07-08 10:11:10 +0100

    add event model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_080628) do
+ActiveRecord::Schema.define(version: 2020_07_08_090100) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
