842aea86b79c9e075f7c6b9209d6e4aca563f1a7
Mupa1 <mupasmail@gmail.com>
2020-07-08 10:33:39 +0200

    add user model, controller, view and session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    def new
+    end
+    def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user
+        session[:user_id] = user.id
+        flash[:notice] = 'You have successfully logged in'
+        redirect_to user_path(user)
+      else
+        flash.now[:alert] = 'There was something wrong with your login information'
+        render 'new'
+      end
+    end
+    def destroy
+      session[:user_id] = nil
+      flash[:notice] = 'You have logged out'
+      redirect_to login_path
+    end
+end
